16f88e081a5cc0e97531e05648f39a6600fe769a
Anna <annamura833@gmail.com>
2021-06-04 09:17:29 +0200

    Aggiungere i file di progetto.

---
+++ Prova6-AnnaMura/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prova6_AnnaMura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
